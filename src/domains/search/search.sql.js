5f1307280ac3e618a351a1e9b72b6472643454f2
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-10-27 07:06:13 +0900

    feat: id 반환 삭제 #86

--- src/domains/search/search.sql.js
+++ src/domains/search/search.sql.js
@@ -1,9 +1,9 @@
 export const searchSql = 
-"SELECT s.id, credit, name, CASE WHEN us.user_id IS NOT NULL THEN 1 ELSE 0 END AS bookmark FROM subject s "
+"SELECT credit, name, CASE WHEN us.user_id IS NOT NULL THEN 1 ELSE 0 END AS bookmark FROM subject s "
 + "JOIN user_subject us ON s.name = us.subject_name AND user_id = ? "
 + "WHERE type = ? ORDER BY s.id;"
 
 export const searchNameSql = 
-"SELECT s.id, credit, name, CASE WHEN us.user_id IS NOT NULL THEN 1 ELSE 0 END AS bookmark FROM subject s "
+"SELECT credit, name, CASE WHEN us.user_id IS NOT NULL THEN 1 ELSE 0 END AS bookmark FROM subject s "
 + "JOIN user_subject us ON s.name = us.subject_name AND user_id = ? "
 + "WHERE type = ? AND name REGEXP ? ORDER BY s.id;"
