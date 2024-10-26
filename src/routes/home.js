42c5d25e83381b19f98141b766ef4ebd25ddad57
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-10-26 22:06:02 +0900

    refactor: index.js 충돌 해결 #35

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,10 +1,8 @@
-import express, { Router } from "express";
+import express from "express";
 import { homeController } from "../domains/home/home.controller.js";
 import { tokenAuth } from "../middlewares/token.auth.js";
 
-const router = express.Router();
+export const homeRoute = express.Router();
 
 // 토큰 인증 미들웨어를 사용한 후에 homeController 호출
-routerouter.get("/home", tokenAuth, homeController);
-
-export default router;
+homeRoute.get("", tokenAuth, homeController);
