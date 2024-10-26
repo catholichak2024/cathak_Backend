6de88f5f30f8a4ff48a28cd7f74e4b55fb44748e
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-10-26 16:20:54 +0900

    feat: bookmark 표시 추가 #47

--- src/domains/spec/spec.sql.js
+++ src/domains/spec/spec.sql.js
@@ -1,5 +1,6 @@
 export const bcSql = 
-"SELECT credit, name FROM subject JOIN user_subject us ON subject.name = us.subject_name "
+"SELECT credit, name, CASE WHEN us.user_id IS NOT NULL THEN 1 ELSE 0 END AS bookmark FROM subject "
++ "JOIN user_subject us ON subject.name = us.subject_name "
 + "WHERE type_spec='기초교양' AND user_id = ?;"
 export const bcCredit = 
 "SELECT SUM(credit) AS sum FROM subject JOIN user_subject us ON subject.name = us.subject_name "
