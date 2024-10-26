b901e4515d2ad1574a2e3cf55957d84de2d08724
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-10-27 06:49:00 +0900

    feat: userId 추가 #84

--- src/domains/search/search.repository.js
+++ src/domains/search/search.repository.js
@@ -3,18 +3,18 @@ import { status } from "../../response.status.js";
 import { pool } from "../../db.config.js";
 import { searchSql, searchNameSql } from "./search.sql.js";
 
-export const searchRepo = async (type, name) => {
+export const searchRepo = async (userId, type, name) => {
     const conn = await pool.getConnection();
     try {
         if(typeof type == "undefined") {
             throw new BaseError(status.PARAMETER_IS_WRONG);
         }
         if(typeof name == "undefined"){
-            const major = await pool.query(searchSql, type);
+            const major = await pool.query(searchSql, [userId, type]);
             conn.release();
             return major;
         }else{
-            const major = await pool.query(searchNameSql, [type, name]);
+            const major = await pool.query(searchNameSql, [userId, type, name]);
             conn.release();
             return major;
         }
