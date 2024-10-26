69c9a2f2bcb509e196a7c453ffebb7be4cb813d9
chanmin9929 <snappy0707@naver.com>
2024-10-26 23:45:33 +0900

    실패#

--- src/domains/home/home.controller.js
+++ src/domains/home/home.controller.js
@@ -5,15 +5,18 @@ export const homeController = async (req, res, next) => {
         const userId = req.decoded.userId; // 미들웨어에서 가져온 사용자 ID
         
         // 학점 정보 가져오기
-        const homeData = await homeService.getAllCourses(userId);
+        const homeData = await homeService.getHomeData(userId);
 
 
         res.status(200).json({
             message: "All courses fetched successfully",
             data: {
-                culturalCourses: homeData.cultural, // 교양 학점
-                majorFoundationCourses: homeData.majorFoundation, // 전공기초 학점
-                majorCourses: homeData.major, // 전공 학점
+                culturalCourses: homeData.culturalCredits, // 교양 학점
+                majorFoundationCourses: homeData.majorFoundationCredits, // 전공기초 학점
+                majorCourses: homeData.majorCredits, // 전공 학점
+                totalCredits: homeData.totalCredits,  // 총 학점
+                totalGPA: homeData.totalGPA, // 전체 평점
+                majorGPA: homeData.majorGPA // 전공 평점
             }
               
         });
